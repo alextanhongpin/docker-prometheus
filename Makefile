b8cbec83ef2bfda3fe64fcaef56b39e8929fd5f7
alextanhongpin <alextan220990@gmail.com>
2023-01-12 00:29:08 +0800

    chore: add build info

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
+export
+REVISION := $(shell git rev-parse --short HEAD)
+BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
+VERSION := $(shell cat VERSION)
+
+
+run:
+	@go run main.go
+
+
 up:
 	@docker-compose up -d
+
 
 down:
 	@docker-compose down
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"runtime"
 	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -30,7 +32,7 @@ var (
 			Name: "http_requests_total",
 			Help: "Number of get requests",
 		},
-		[]string{"path"},
+		[]string{"method", "path"},
 	)
 
 	responseStatus = prometheus.NewCounterVec(
@@ -51,9 +53,10 @@ var (
 )
 
 func init() {
-	prometheus.Register(totalRequests)
-	prometheus.Register(responseStatus)
-	prometheus.Register(httpDuration)
+	prometheus.MustRegister(totalRequests)
+	prometheus.MustRegister(responseStatus)
+	prometheus.MustRegister(httpDuration)
+	prometheus.MustRegister(NewCollector("myapp"))
 }
 
 func main() {
@@ -75,7 +78,7 @@ func prometheusMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(rw, r)
 
 		responseStatus.WithLabelValues(strconv.Itoa(rw.statusCode)).Inc()
-		totalRequests.WithLabelValues(path).Inc()
+		totalRequests.WithLabelValues(r.Method, path).Inc()
 		timer.ObserveDuration()
 	})
 }
@@ -86,4 +89,32 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func greetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world"))
+}
+
+// NewCollector returns a collector that exports metrics about current version
+// information.
+// https://pkg.go.dev/github.com/prometheus/client_golang/prometheus#Gauge:~:text=NewGaugeFunc%20is%20a%20good%20way%20to%20create%20an%20%E2%80%9Cinfo%E2%80%9D%20style%20metric%20with%20a%20constant%20value%20of%201.%20Example%3A%20https%3A//github.com/prometheus/common/blob/8558a5b7db3c84fa38b4766966059a7bd5bfa2ee/version/info.go%23L36%2DL56
+//
+// This will appear as myapp_build_info
+//
+// Output:
+// myapp_build_info{branch="master", goversion="go1.19.4", instance="host.docker.internal:2112", job="myapp", revision="e69834f", version="0.0.1"}
+func NewCollector(program string) prometheus.Collector {
+	return prometheus.NewGaugeFunc(
+		prometheus.GaugeOpts{
+			Namespace: program,
+			Name:      "build_info",
+			Help: fmt.Sprintf(
+				"A metric with a constant '1' value labeled by version, revision, branch, and goversion from which %s was built.",
+				program,
+			),
+			ConstLabels: prometheus.Labels{
+				"version":   os.Getenv("VERSION"),
+				"revision":  os.Getenv("REVISION"),
+				"branch":    os.Getenv("BRANCH"),
+				"goversion": runtime.Version(),
+			},
+		},
+		func() float64 { return 1 },
+	)
 }
